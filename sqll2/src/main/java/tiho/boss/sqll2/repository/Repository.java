671c686d9257d13b9259ba0330c729ef076ff36e
tihoboss <159692983+tihoboss@users.noreply.github.com>
2025-03-19 19:28:24 +0300

    Update Repository.java

--- sqll2/src/main/java/tiho/boss/sqll2/repository/Repository.java
+++ sqll2/src/main/java/tiho/boss/sqll2/repository/Repository.java
@@ -2,17 +2,17 @@ package tiho.boss.sqll2.repository;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
 import tiho.boss.sqll2.Persons;
-
 import java.util.List;
 
-@org.springframework.stereotype.Repository
-public class Repository {
+@Repository
+public class SRepository {
     @PersistenceContext
     private EntityManager entityManager;
 
     public List<Persons> getPersonsByCity(String city) {
-        return entityManager.createQuery("select p from Persons p where p.city_of_living = :city order by p.city_of_living", Persons.class)
+        return entityManager.createQuery("select p from Persons p where p.cityOfLiving = :city order by p.cityOfLiving", Persons.class)
                 .setParameter("city", city)
                 .getResultList();
     }
